9149c2203cbf232245f249a6c67508e13c066723
otissv <otissv@gmail.com>
2015-01-09 14:20:59 +0000

    changed testing to mocha annd testem

--- gulpFile.js
+++ gulpFile.js
@@ -4,23 +4,30 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 
+
 // Include gulp plugins
 var nodemon = require('gulp-nodemon');
 var browserSync = require('browser-sync');
-var jshint =     require('gulp-jshint');
-var jest = require('gulp-jest');
+var jshint = require('gulp-jshint');
+var jshintStylish = require('jshint-stylish');
+var plumber = require('gulp-plumber');
+var testem = require('gulp-testem');
+var mocha = require('gulp-mocha');
+var notify = require('gulp-notify');
 
 //config
 var config = {
-  tests: './__tests__/**.*.js',
-  scripts: {
-    client: './public/**/*.js',
-    server: './server/**/*.js'
-  },
+  tests: './__tests__/**/*.js',
+  testem: './__tests__/client/*.js',
+  scripts: [
+    './public/**/*.js',
+    './server/**/*.js',
+    './__tests__/**/*.js'
+  ],
   nodemon: {
     script: './bin/www',
     env: { 'NODE_ENV': 'development' },
-    watch: ['server/**/*.*']
+    watch: 'server/**/*.*'
   },
   browserSync: {
     files: ['public/**/*.*'],
@@ -33,17 +40,29 @@ var config = {
 
 
 // Error handling
-var onError = function (err) {
-  gutil.beep();
-  console.log(err);
+// var onError = function (err) {
+//   gutil.beep();
+//   console.log(err);
+// };
+
+var gulp_src = gulp.src;
+gulp.src = function() {
+  return gulp_src.apply(gulp, arguments)
+  .pipe(plumber(function(error) {
+    // Output an error message
+    gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
+    // emit the end event, to properly end the task
+    this.emit('end');
+  })
+);
 };
 
 
 // Lint scripts
 gulp.task('lintScripts', function () {
-  gulp.src(['gulpFile.js', config.tests, config.scripts.client, config.scripts.server])
-  .pipe(jshint())
-  .pipe(jshint.reporter('default'));
+  gulp.src(['./**/*.js', '!./node_modules/**/*.js'])
+  .pipe(jshint('.jshintrc'))
+  .pipe(jshint.reporter('jshint-stylish'))
 });
 
 
@@ -73,7 +92,7 @@ gulp.task('nodemon', function(cb) {
 });
 
 
-//  Restart browser on file change
+// Restart browser on file change
 gulp.task('browser-sync', ['nodemon'], function() {
   browserSync.init({
     files: config.browserSync.files,
@@ -83,30 +102,58 @@ gulp.task('browser-sync', ['nodemon'], function() {
   });
 });
 
-gulp.task('jest', function () {
-  return gulp.src('__tests__').pipe(jest({
-    testDirectoryName: '__tests__',
-    moduleFileExtensions: [
-      'js',
-      'json',
-      'jsx'
-    ]
+
+// Test server
+gulp.task('coverage', function () {
+  var coverageServer = http.createServer(function (req, resp) {
+    req.pipe(fs.createWriteStream('coverage.json'));
+    resp.end();
+  });
+
+  var port = 7358;
+  coverageServer.listen(port);
+  console.log('Coverage Server Started on port', port);
+});
+
+
+// Run browser tests
+gulp.task('testem', function (done) {
+  gulp.src(config.testem, {read: false})
+  .pipe(testem({configFile: 'testem.json'}));
+});
+
+// Run server tests
+gulp.task('mocha', function () {
+  return gulp.src(config.tests, {read: false})
+  .pipe(mocha({reporter: 'nyan'}))
+  .on('error', notify.onError({
+    message: 'Error: <%= error.message %>',
+    sound: false // deactivate sound?
   }));
 });
 
 
 // Watch files for changes
 gulp.task('watch', function () {
-  gulp.watch([config.scripts.client, config.tests, config.scripts.server], ['lintScripts']);
+  gulp.watch(['./**/*.js', '!./node_modules/**/*.js'], ['lintScripts']);
 });
 
-// Default gulp
+// Run continus tests
+gulp.task('test', ['mocha'], function () {
+  gulp.watch([config.tests], ['mocha']);
+});
+
+
+// Default gulp task
 gulp.task('default', [
   'lintScripts',
-  'browser-sync',
   'watch'
 ]);
 
-// Build app for distribution
+// Build app for distribution.
 gulp.task('build', []);
-gulp.task('test', ['lintScripts','jest']);
+gulp.task('serve', [
+  'lintScripts',
+  'mocha',
+  'browser-sync'
+]);
